ef97754b18c605f0e8cfd80f4d51579710136d06
JENO87 <jens.c.norell@gmail.com>
2021-05-22 20:39:23 +0200

    fixed SearchNetworkDataSource.kt. Now we can use it without lat/lon

--- app/src/main/java/com/trkkr/trkkrclean/api/ors/search/SearchNetworkDataSource.kt
+++ app/src/main/java/com/trkkr/trkkrclean/api/ors/search/SearchNetworkDataSource.kt
@@ -1,9 +1,14 @@
 package com.trkkr.trkkrclean.api.ors.search
 import javax.inject.Inject
 
-class SearchNetworkDataSource @Inject constructor(
-    private val searchService: SearchService
-) : SearchDataSource {
-    override suspend fun fetchSearchResult(api_key: String, text: String, lon: String, lat: String)
-    = searchService.searchWithLatLon(api_key, text, lon, lat)
+class SearchNetworkDataSource @Inject constructor(private val searchService: SearchService) : SearchDataSource {
+
+    override suspend fun fetchSearchResult(api_key: String, text: String, lon: String?, lat: String?): SearchDto {
+        return if (lon == null || lat == null) {
+            searchService.searchWithoutLatLon(api_key, text)
+        } else {
+            searchService.searchWithLatLon(api_key, text, lon, lat)
+        }
+    }
+
 }
