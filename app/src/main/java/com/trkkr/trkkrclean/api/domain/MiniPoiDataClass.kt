58f2bee29ce929db8b5423b6e7bce17cac50302d
Jens <jens.c.norell@gmail.com>
2021-06-06 15:43:43 +0200

    fixed fab positions

--- app/src/main/java/com/trkkr/trkkrclean/api/domain/MiniPoiDataClass.kt
+++ app/src/main/java/com/trkkr/trkkrclean/api/domain/MiniPoiDataClass.kt
@@ -1,12 +1,18 @@
 package com.trkkr.trkkrclean.api.domain
 
+import android.widget.ImageView
 import com.google.gson.annotations.SerializedName
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.trkkr.trkkrclean.api.trkkr.ImageDto
 
 data class MiniPoiDataClass (
 
+    var images: List<ImageView>? = null,
     var name: String? = null,
     var category: String? = null,
     var distance: String? = null,
-    var open: Boolean? = null
+    var open: Boolean? = null,
+    var myPosition: LatLng? = null,
+    var poiPosition: LatLng? = null,
+    var number: String? =null
 )
